c0d43e3774e6f35883a7dbe8c3f852a2e7b743a0
Reyzaldy Indra <ireyzaldi@gmail.com>
2018-11-27 16:39:56 +0700

    update 16:39

--- app/src/main/java/app/piruma_java/FormActivity.java
+++ app/src/main/java/app/piruma_java/FormActivity.java
@@ -1,8 +1,10 @@
 package app.piruma_java;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -10,11 +12,13 @@ import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import app.piruma_java.Activity.LoginActivity;
 import app.piruma_java.Activity.SignupActivity;
+import app.piruma_java.selectroom.*;
 import app.piruma_java.network.VolleyNetwork;
 
 public class FormActivity extends AppCompatActivity {
@@ -28,6 +32,13 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
         arrow_back = findViewById(R.id.arrow_back);
+        arrow_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(FormActivity.this, SelectRoomActivity.class);
+                startActivity(intent);
+            }
+        });
         txOrderSchedule = findViewById(R.id.tx_order_schedule);
         txOrderRoom = findViewById(R.id.tx_order_room);
         etPic = findViewById(R.id.et_pic);
@@ -46,7 +57,7 @@ public class FormActivity extends AppCompatActivity {
         try{
             order.put("penanggung_jawab",etPic.getText().toString());
             order.put("departemen",etJurusan.getText().toString());
-            order.put("nim",etKeperluan.getText().toString());
+//            order.put("nim",etKeperluan.getText().toString());
             order.put("telepon",etNoHP.getText().toString());
             order.put("keterangan",etKeterangan.getText().toString());
 
